b94b106b8982c75168116b2d59bed80d981826d0
Compilenix <compilenix@gmail.com>
2018-12-26 17:06:58 +0100

    format code according to JsStandard

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict'
 
-let Netmask = require("netmask").Netmask;
-let FtpScanner = require("./lib/FtpScanner.js");
+let Netmask = require('netmask').Netmask
+let FtpScanner = require('./lib/FtpScanner.js')
 
-let outfile = "tmp.html";
-let countOfParalellJobWorker = 400;
-let ipAddressBlock = new Netmask("151.217.0.0/16"); // CCC Event IPv4 Subnet
-let ipAddresses = [];
+let outfile = 'tmp.html'
+let countOfParalellJobWorker = 400
+let ipAddressBlock = new Netmask('151.217.0.0/16') // CCC Event IPv4 Subnet
+let ipAddresses = []
 
 ipAddressBlock.forEach((ip) => {
-	ipAddresses.push(ip);
-});
+  ipAddresses.push(ip)
+})
 
-let ftpScanner = new FtpScanner(ipAddresses, outfile, undefined);
-ftpScanner.WriteOutputHeaderHtml("compilenix.org", "ftpscan", "archive");
+let ftpScanner = new FtpScanner(ipAddresses, outfile, undefined)
+ftpScanner.WriteOutputHeaderHtml('compilenix.org', 'ftpscan', 'archive')
 
 for (var i = countOfParalellJobWorker; i > 0; i--) {
-	if (ipAddresses.length > 0) {
-		ftpScanner.StartProcessingOfNextJob();
-	}
+  if (ipAddresses.length > 0) {
+    ftpScanner.StartProcessingOfNextJob()
+  }
 }
