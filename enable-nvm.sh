52077ebca5bc456f0f6ad27f330d6b447fca57d9
Compilenix <compilenix@gmail.com>
2018-12-27 16:55:11 +0100

    remove "./init.sh" from "enable-nvm.sh" script

--- enable-nvm.sh
+++ enable-nvm.sh
@@ -4,7 +4,6 @@ export NVM_DIR="$(realpath $HOME/.nvm)"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || {
   echo "you need nvm (https://github.com/creationix/nvm)\ncurl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash"; exit 1
 }
-[ ! -d "./node_modules" ] && ./init.sh
 
 nvm --version 1>/dev/null 2>&1
 [ $? -ne 0 ] && echo "you need nvm (https://github.com/creationix/nvm)\ncurl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash" && exit 1
